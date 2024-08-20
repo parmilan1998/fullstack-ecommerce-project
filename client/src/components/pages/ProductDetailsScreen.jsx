c2fbcb3e958c81ff656b2456f07ec1a3c47bad53
parmilan1998 <parmilancs2024@gmail.com>
2024-08-20 17:26:56 +0530

    Product

--- client/src/components/pages/ProductDetailsScreen.jsx
+++ client/src/components/pages/ProductDetailsScreen.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ProductImageGallery from "../organisms/ProductImageGallery";
 import SingleProductInfo from "../organisms/SingleProductInfo";
 import RelatedItem from "../organisms/RelatedItem";
+import Title from "../atoms/Title";
 
 const ProductDetailsScreen = ({ item }) => {
   return (
     <div className="mx-auto">
+      <div className="py-3">
+        <Title props="Product Details" />
+      </div>
       <div className="grid grid-cols-7 gap-6">
         <div className="col-span-4">
           <ProductImageGallery />
