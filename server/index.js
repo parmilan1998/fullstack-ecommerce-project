f8d3000b73d52c2edfdbc014f25ec7cfb7dff875
parmilan1998 <parmilancs2024@gmail.com>
2024-08-26 10:35:05 +0530

    error fix

--- server/index.js
+++ server/index.js
@@ -16,9 +16,9 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 // TODO: sample api
-app.use("/", (req, res) => {
-  res.send("Welcome to the Ecommerce Shopping API!🚀🚀🚀");
-});
+// app.use("/", (req, res) => {
+//   res.send("Welcome to the Ecommerce Shopping API!🚀🚀🚀");
+// });
 
 //TODO: import routes
 import categoryRoute from "./src/routes/category.routes.js";
